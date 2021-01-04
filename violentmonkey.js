0a2992bf4d935a30cc952dcfd37fb447fb98ddaf
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2021-01-04 23:23:06 +0200

    Version 1.7

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Adblocker for Anacon
 // @namespace       github.com/kiriles90
-// @version         1.6
+// @version         1.7
 // @date            2021-01-04
 // @author          github.com/kiriles90
 // @updateURL       https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
@@ -12,20 +12,19 @@
 // ==/UserScript==
 const exo = document.querySelector("[src*='exo.jpg']"),
       submit = document.querySelector("[name='submit']"),
-      submitall = document.querySelectorAll("[name='submit']")[0],
       cmd = document.querySelector("[name='cmd']"),
       hosted = document.querySelector("[name='hosted_button_id']");
 exo ? (exo.style.opacity = 0, setTimeout(function(){ exo.click(); }, 1000)) : null;
-submit ? submitall.remove() : null;
+submit ? submit.remove() : null;
 cmd ? cmd.remove() : null;
 hosted ? hosted.remove() : null;
 var observer = new MutationObserver(function (mutations, me) {
-    var retries = document.querySelectorAll(".adsbygoogle").length !== 0 ? document.querySelectorAll(".adsbygoogle").length : document.querySelectorAll("[name^='bdvifrmloc']").length;
-    const canvas = document.querySelector(".adsbygoogle"),
-          canvas2 = document.querySelectorAll("[name^='bdvifrmloc']")[0],
+    const canvas = document.querySelectorAll(".adsbygoogle"),
+          canvas2 = document.querySelectorAll("[name^='bdvifrmloc']"),
           fpplayer = document.querySelector(".fp-player");
-    canvas ? canvas.remove() : null;
-    canvas2 ? canvas2.remove() : null;
+    var retries = canvas.length !== 0 ? canvas.length : canvas2.length;
+    canvas[0] ? canvas[0].remove() : null;
+    canvas2[0] ? canvas2[0].remove() : null;
     fpplayer ? fpplayer.style.backgroundColor = "#000" : null;
     retries < 1 ? me.disconnect() : retries = retries - 1;
     return;
