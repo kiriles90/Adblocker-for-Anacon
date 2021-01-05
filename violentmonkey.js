8332ee36f4d617d481774da0820d35f163f5cd47
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2021-01-06 01:32:24 +0200

    Version 2.2

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name            Adblocker for Anacon
 // @namespace       github.com/kiriles90
-// @version         2.1
-// @date            2021-01-05
+// @version         2.2
+// @date            2021-01-06
 // @author          github.com/kiriles90
 // @updateURL       https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
 // @downloadURL     https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
@@ -17,16 +17,17 @@ exo ? (exo.style.opacity = 0, setTimeout(function(){ exo.click(); }, 1000)) : nu
 var observer = new MutationObserver(function (mutations, me) {
     const cmd = document.querySelector("[name='cmd']"),
           hosted = document.querySelector("[name='hosted_button_id']"),
-          canvas = document.querySelectorAll(".adsbygoogle"),
-          canvas2 = document.querySelectorAll("[name^='bdvifrmloc']"),
-          fpplayer = document.querySelector(".fp-player");
-    var retries = canvas.length !== 0 ? canvas.length : canvas2.length;
+          fpplayer = document.querySelector(".fp-player"),
+          ads = [
+              document.querySelectorAll(".adsbygoogle"), 
+              document.querySelectorAll("[name^='bdvifrmloc']"), 
+              document.querySelectorAll("[href^='https://ellinus']")
+          ];
     cmd ? cmd.remove() : null;
     hosted ? hosted.remove() : null;
-    canvas[0] ? canvas[0].remove() : null;
-    canvas2[0] ? canvas2[0].remove() : null;
     fpplayer ? fpplayer.style.backgroundColor = "#000" : null;
-    retries < 1 ? me.disconnect() : retries = retries - 1;
+    Array.from(ads).forEach(ad => Array.from(ad).forEach(adoc => adoc.remove()));
+    ads[0].length === 0 && ads[1].length === 0 && ads[2].length === 0 ? me.disconnect() : null;
     return;
 });
 observer.observe(document, {
