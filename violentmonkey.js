c2168d1bbecd864c9210bec750ee2cf40daae3ab
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2021-08-15 00:56:03 +0300

    Version 3.2

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Adblocker for Anacon
 // @namespace       github.com/kiriles90
-// @version         3.1
+// @version         3.2
 // @date            2021-08-15
 // @author          github.com/kiriles90
 // @updateURL       https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
@@ -13,8 +13,9 @@
 const ads = document.querySelectorAll("center, .google-center-div, .google-auto-placed, .mys-wrapper, root_template_div, .adsbygoogle, #banner"),
       content = document.querySelector(".content"),
       fpplayer = document.querySelector(".fp-player"),
-      exo = document.querySelector(".fp-play");
+      exo = document.querySelector("[src*='exo.jpg']");
+      exo2 = document.querySelector(".fp-play");
 ads ? Array.from(ads).forEach(ad => ad.remove()) : null;
 content ? (content.parentElement.style = 'height:100vh;width:100%;display:table;', content.style = 'height:100%;display:table-cell;vertical-align:middle;') : null;
 fpplayer ? fpplayer.style.backgroundColor = "#000" : null;
-exo ? (exo.style.opacity = 0, setTimeout(function(){ exo.click(); }, 1000)) : null;
+exo ? (exo.style.opacity = 0, setTimeout(function(){ exo.click(); exo2.click(); ads ? Array.from(ads).forEach(ad => ad.remove()) : null;}, 1000), setTimeout(function(){ exo2.click(); }, 5000)) : null;
