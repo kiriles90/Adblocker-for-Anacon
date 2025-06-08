41c36de11e90f4f3ff4e52f4a88070db058d9839
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2025-06-08 22:20:59 +0300

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Adblocker for Anacon
 // @namespace   github.com/kiriles90
-// @version     3.0
-// @date        2025-06-10
+// @version     3.2
+// @date        2025-06-11
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
@@ -11,44 +11,28 @@
 // @grant       none
 // ==/UserScript==
 (() => {
-    const ads = [
-        'center',
-        '.adsbygoogle',
-        '.donate-button',
-        '.fc-message-root',
-        '.ft-container ft-left-pos',
-        '.ipr-container'
-    ].join(',');
+    const ads = 'center,.adsbygoogle,.donate-button,.fc-message-root,.ft-container.ft-left-pos,.ipr-container';
     const clean = () => {
-        document.querySelectorAll(ads).forEach(el => el.remove());
+        const adEls = document.querySelectorAll(ads);
+        adEls.forEach(el => el.remove());
         const content = document.querySelector('.content');
-        if (content) {
+        if (content?.parentElement) {
             Object.assign(content.parentElement.style, {
-                height: '100vh',
-                width: '100%',
-                display: 'table'
+                height: '100vh', width: '100%', display: 'table'
             });
             Object.assign(content.style, {
-                height: '100%',
-                display: 'table-cell',
-                verticalAlign: 'middle'
+                height: '100%', display: 'table-cell', verticalAlign: 'middle'
             });
         }
         const player = document.querySelector('.fp-player');
-        if (player && player.style.backgroundColor !== '#000') {
-            player.style.backgroundColor = '#000';
-        }
+        if (player) player.style.backgroundColor = '#000';
         const exoImg = document.querySelector("[src*='exo.jpg']");
-        if (exoImg && exoImg.style.opacity !== '0') {
-            exoImg.style.opacity = '0';
-        }
-        setTimeout(() => {
+        if (exoImg) exoImg.style.opacity = '0';
+        requestAnimationFrame(() => {
             document.querySelector("[src*='exo.jpg']")?.click();
-            const playBtn = document.querySelector('.fp-play');
-            if (playBtn?.classList.contains('fp-visible')) {
-                playBtn.click();
-            }
-        }, 1000);
+            document.querySelector('.fp-play.fp-visible')?.click();
+        });
     };
-    [0, 2000, 4000, 20000].forEach(delay => setTimeout(clean, delay));
+    const schedule = [0, 2000, 4000, 20000];
+    schedule.forEach(delay => setTimeout(() => requestAnimationFrame(clean), delay));
 })();
