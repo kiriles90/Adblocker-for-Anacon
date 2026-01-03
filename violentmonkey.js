8ac57e2a291a25efb12019da141d4aa254b9d659
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2026-01-03 16:09:58 +0200

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Adblocker for Anacon
 // @namespace   github.com/kiriles90
-// @version     4.2
-// @date        2025-10-18
+// @version     4.4
+// @date        2026-01-03
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
@@ -11,6 +11,11 @@
 // @grant       none
 // ==/UserScript==
 (() => {
+    const m3u8 = document.querySelector('#live-stream source') ? document.querySelector('#live-stream source').src : null;
+    if (m3u8 && m3u8.includes('.m3u8')) {
+        window.location.href = m3u8;
+       return;
+    }
     const adsSelector = 'center,.adsbygoogle,.donate-button,.fc-message-root,.ft-container.ft-left-pos,.ipr-container';
     function cleanPage() {
         document.querySelectorAll(adsSelector).forEach(el => el.remove());
@@ -25,8 +30,6 @@
             contentStyle.display = 'table-cell';
             contentStyle.verticalAlign = 'middle';
         }
-        const player = document.querySelector('.fp-player');
-        if (player) player.style.backgroundColor = '#000';
         const exoImg = document.querySelector("[src*='play.webp']");
         if (exoImg) exoImg.style.opacity = '0';
         requestAnimationFrame(() => {
@@ -34,7 +37,7 @@
             document.querySelector('.vjs-big-play-button')?.click();
         });
     }
-    [0, 1000, 2000, 4000, 6000, 8000, 20000].forEach(delay =>
+    [0, 1000, 2000, 4000, 8000, 16000].forEach(delay =>
         setTimeout(() => requestAnimationFrame(cleanPage), delay)
     );
 })();
