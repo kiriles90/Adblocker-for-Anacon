2e846054417617c0958365a2619630ad398304e8
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2026-05-16 15:50:08 +0300

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Adblocker for Anacon
 // @namespace   github.com/kiriles90
-// @version     5.8
-// @date        2026-05-02
+// @version     5.9
+// @date        2026-05-16
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Adblocker-for-Anacon/master/violentmonkey.js
